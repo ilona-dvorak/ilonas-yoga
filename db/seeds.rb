0998ded53c356022fd3bc3537d82bc50ee3f58ab
Malin Schadel <malin.schadel@gmail.com>
2021-01-26 21:47:19 +0100

    working on home-page and adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+User.destroy_all
+Yogaclass.destroy_all
+
+puts "Creating users..."
+
+@user_one = User.new(email: "malin@me.org", password: "12345678")
+
+puts "Creating yogaclasses..."
+
+@yoga_one = Yogaclass.create!(title: "Yoga in the Park", price: "10 Euros", class_type: "Yoga-Beginner", address: "Wiener Stadtpark, Parkring 1, 1010 Wien, Austria", duration: 0.45)
+@yoga_one.user = @user_one
+
+puts "Created #{@yoga_one.title}!"
